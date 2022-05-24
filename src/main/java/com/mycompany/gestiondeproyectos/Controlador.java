7d36cbb56e87ba5d545666df269e10f0cfd97226
HappyMan17 <sebasgrisales16@gmail.com>
2022-05-23 21:56:50 -0500

    version 2

--- src/main/java/com/mycompany/gestiondeproyectos/Controlador.java
+++ src/main/java/com/mycompany/gestiondeproyectos/Controlador.java
@@ -27,25 +27,106 @@ public class Controlador {
             
         System.out.println("Company management of "+store.getName());
         System.out.println("What do you want to do?");
-        System.out.println("1. product management.");
-        System.out.println("2. supplier management.");
-        System.out.println("3. client management.");
-        System.out.println("4. leave.");
+        System.out.println("1. Product management.");
+        System.out.println("2. Supplier management.");
+        System.out.println("3. Client management.");
+        System.out.println("4. Money register admin.");
+        System.out.println("5. leave.");
         opcion = sc.nextByte();
         switch(opcion){
-            case 1: clientMenu();
+            case 1: productMenu();
                 break;
-            case 2: System.out.println("Paso 2");
+            case 2: supplierMenu();
+                System.out.println("Paso 2");
                 break;
-            case 3: System.out.println("Paso 3");
+            case 3: clientMenu();
+                System.out.println("Paso 3");
+                break;
+            case 4: moneyRegisterMenu();
+                System.out.println("Paso 3");
                 break;
         }
         
-        }while (opcion != 4);
+        }while (opcion != 5);
         System.out.println("Gracias!!!!");
     }
     
+    public void productMenu(){
+        byte opcion = 0;
+        Scanner sc = new Scanner(System.in);
+        do{ 
+            
+            System.out.println("Company management of "+store.getName());
+            System.out.println("What do you want to do?");
+            System.out.println("1. Add a product.");
+            System.out.println("2. Remove a product.");
+            System.out.println("3. See products.");
+            System.out.println("4. leave.");
+            opcion = sc.nextByte();
+            switch(opcion){
+                case 1: store.setProduct();
+                    System.out.println("Product add");
+                    break;
+                case 2: store.removeProduct();
+                    System.out.println("Paso 2");
+                    break;
+                case 3: store.seeProducts();
+                    break;
+            }
+        
+        }while (opcion != 4);
+    }
     public void clientMenu(){
+        byte opcion = 0;
+        Scanner sc = new Scanner(System.in);
+        do{ 
+            
+            System.out.println("Company management of "+store.getName());
+            System.out.println("What do you want to do?");
+            System.out.println("1. Add a product.");
+            System.out.println("2. Remove a product.");
+            System.out.println("3. See products.");
+            System.out.println("4. leave.");
+            opcion = sc.nextByte();
+            switch(opcion){
+                case 1: store.setProduct();
+                    System.out.println("Product add");
+                    break;
+                case 2: store.removeProduct();
+                    System.out.println("Paso 2");
+                    break;
+                case 3: store.seeProducts();
+                    break;
+            }
+        
+        }while (opcion != 4);
+    }
+    public void moneyRegisterMenu(){
+        byte opcion = 0;
+        Scanner sc = new Scanner(System.in);
+        do{ 
+            
+            System.out.println("Company management of "+store.getName());
+            System.out.println("What do you want to do?");
+            System.out.println("1. Add a product.");
+            System.out.println("2. Remove a product.");
+            System.out.println("3. See products.");
+            System.out.println("4. leave.");
+            opcion = sc.nextByte();
+            switch(opcion){
+                case 1: store.setProduct();
+                    System.out.println("Product add");
+                    break;
+                case 2: store.removeProduct();
+                    System.out.println("Paso 2");
+                    break;
+                case 3: store.seeProducts();
+                    break;
+            }
+        
+        }while (opcion != 4);
+    }
+    public void supplierMenu(){
         byte opcion = 0;
         Scanner sc = new Scanner(System.in);
         do{ 
